6b6da55f210da08859672c47705e0b603b1d907a
Florian Le Saint <florian.lesaint@brest-opencampus.com>
2021-12-13 10:29:28 +0100

    Initial commit

---
+++ src/main/java/com/github/flols/demoinitialzr/DemoInitialzrApplication.java
@@ -0,0 +1,13 @@
+package com.github.flols.demoinitialzr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInitialzrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoInitialzrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/flols/demoinitialzr/DemoInitialzrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.flols.demoinitialzr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInitialzrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
